1f786c5f0c3d6c33bdbfdd3e9be5baadfc58d6de
abut40052@gmail.com <abut40052@gmail.com>
2024-11-29 17:51:41 +0530

    Coffees Server Create Complate again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
